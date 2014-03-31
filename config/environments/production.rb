cbf36562de5e9ee74bcdbc066b8797f6c5c5e65e
Devin Wieland <devinwieland@gmail.com>
2014-03-30 20:31:52 -0500

    trying to get css working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Blog::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
@@ -20,14 +25,14 @@ Blog::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
